f2551efb7a1a981e189b5c03e61a40419f963a9b
nailsonseat <aadarshv3@gmail.com>
2023-01-26 23:14:01 +0530

    anonymous auth functionality

--- lib/services/authenticate.dart
+++ lib/services/authenticate.dart
@@ -1,4 +1,21 @@
 
-class Authenticate {
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:logging/logging.dart';
 
+class Authenticate {
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  final logger=Logger('Auth logger');
+
+  Future signInAnonymously() async {
+    try {
+      UserCredential result = await _auth.signInAnonymously();
+      User user = result.user!;
+      logger.info('Success');
+      return user;
+    }catch(error){
+      logger.severe('Login Failed');
+      return null;
+    }
+  }
 }
