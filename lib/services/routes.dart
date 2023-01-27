e830b0c97ef9aabd2480a1928929717ce4f8a221
nailsonseat <aadarshv3@gmail.com>
2023-01-27 22:41:33 +0530

    added home route

--- lib/services/routes.dart
+++ lib/services/routes.dart
@@ -1,3 +1,4 @@
+import 'package:acharya_setu/screens/home_page.dart';
 import 'package:acharya_setu/screens/question_bank_browser.dart';
 import 'package:acharya_setu/screens/settings.dart';
 import 'package:acharya_setu/screens/sign_in/sign_in.dart';
@@ -10,10 +11,15 @@ final GoRouter routes = GoRouter(
       builder: (context, state) => const SignInPage(),
       routes: [
         GoRoute(
+          path: 'home',
+          builder: (context, state) => const HomePage(),
+        ),
+        GoRoute(
           path: 'settings',
           builder: (context, state) => const Settings(),
         ),
-        GoRoute(path: 'browser',
+        GoRoute(
+          path: 'browser',
           builder: (context, state) => const QuestionBankBrowser(),
         ),
       ],
