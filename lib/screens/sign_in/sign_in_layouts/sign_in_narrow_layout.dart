860048cc8d1fe574505d9a2f81b06b27e9f73560
nailsonseat <aadarshv3@gmail.com>
2023-01-30 21:05:17 +0530

    teal background for sign in card

--- lib/screens/sign_in/sign_in_layouts/sign_in_narrow_layout.dart
+++ lib/screens/sign_in/sign_in_layouts/sign_in_narrow_layout.dart
@@ -7,23 +7,39 @@ class SignInNarrow extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Colors.amber,
+      color: Colors.teal,
       child: Center(
           child: Column(
-            children: [
-              Expanded(flex: 1,child: Container(),),
-              Expanded(flex: 2,child: Row(
-                children: [
-                  Expanded(flex: 1,child: Container(),),
-                  //Expanded(flex: 10,child: Card(elevation: 20,child: Container(color: Colors.white,),),),
-                  Expanded(flex: 10,child: SignInCard(),),
-                  Expanded(flex: 1,child: Container(),)
-                ],
-              ),),
-              Expanded(flex: 1,child: Container(),),
-            ],
-          )
-      ),
+        children: [
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+          Expanded(
+            flex: 2,
+            child: Row(
+              children: [
+                Expanded(
+                  flex: 1,
+                  child: Container(),
+                ),
+                Expanded(
+                  flex: 10,
+                  child: SignInCard(),
+                ),
+                Expanded(
+                  flex: 1,
+                  child: Container(),
+                )
+              ],
+            ),
+          ),
+          Expanded(
+            flex: 1,
+            child: Container(),
+          ),
+        ],
+      )),
     );
   }
 }
--- lib/screens/sign_in/sign_in_layouts/sign_in_wide_layout.dart
+++ lib/screens/sign_in/sign_in_layouts/sign_in_wide_layout.dart
@@ -3,6 +3,7 @@ import 'package:flutter/material.dart';
 
 class SignInWide extends StatelessWidget {
   const SignInWide({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return Container(
