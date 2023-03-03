9018475997f4d4cdb7a2e56759b5489086654554
nailsonseat <aadarshv3@gmail.com>
2023-03-03 22:47:49 +0530

    initial commit

---
+++ lib/screens/sign_in/sign_in_page_layouts/sign_in_method_layouts/sign_in_google.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class SignInGoogle extends StatelessWidget {
+  const SignInGoogle({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
