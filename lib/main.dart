7ca7f69a32af70f78247061aae55c8835b0030c7
nailsonseat <aadarshv3@gmail.com>
2023-03-10 20:30:23 +0530

    added missing providers and configured firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,14 @@
-import 'package:acharya_setu/providers/country_code_provider.dart';
+import 'package:acharya_setu/providers/login_state_provider.dart';
+import 'package:acharya_setu/providers/user_number_provider.dart';
 import 'package:acharya_setu/services/routes.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
+import 'package:firebase_core/firebase_core.dart';
 
-void main() {
+
+void main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const AcharyaSetuMain());
 }
 
@@ -14,6 +19,7 @@ class AcharyaSetuMain extends StatelessWidget {
   Widget build(BuildContext context) {
     return MultiProvider(
       providers: [
+        ChangeNotifierProvider(create: (_)=>LoginStateProvider()),
         ChangeNotifierProvider(create: (_) => PhoneNumberProvider()),
       ],
       child: Builder(builder: ((context) {
