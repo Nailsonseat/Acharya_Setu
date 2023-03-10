f8e052704e9c89d715346982878d9812456c956d
nailsonseat <aadarshv3@gmail.com>
2023-03-10 20:25:27 +0530

    OTP verification method and configured exceptions

--- lib/services/authentication/authenticate_OTP.dart
+++ lib/services/authentication/authenticate_OTP.dart
@@ -5,14 +5,47 @@ class OTPAuthenticate {
   final FirebaseAuth _auth = FirebaseAuth.instance;
   final logger = Logger('OTP Auth Logger');
   String? phoneNumber;
+  late String _verificationId;
 
   Future signInOTP(String number) async {
     await _auth.verifyPhoneNumber(
       phoneNumber: number,
-      verificationCompleted: (PhoneAuthCredential credential) {},
-      verificationFailed: (FirebaseAuthException exception) {},
-      codeSent: (String verificationId, int? resendToken) {},
-      codeAutoRetrievalTimeout: (String verificationId) {},
+      verificationCompleted: (PhoneAuthCredential credential) async {
+        await _auth.signInWithCredential(credential);
+      },
+      verificationFailed: (FirebaseAuthException exception) {
+        logger.warning('Failed to verify phone number: ${exception.message}');
+      },
+      codeSent: (String verificationId, int? resendToken) {
+        logger.info('OTP sent to phone number: $phoneNumber');
+        _verificationId = verificationId;
+      },
+      codeAutoRetrievalTimeout: (String verificationId) {
+        logger.warning('OTP auto retrieval timeout for phone number: $phoneNumber');
+      },
+      timeout: const Duration(seconds: 30),
     );
+  }
+
+  Future<bool> verifyOTP(String otp) async {
+    try {
+      final PhoneAuthCredential credential = PhoneAuthProvider.credential(
+        verificationId: _verificationId,
+        smsCode: otp,
+      );
+
+      final UserCredential userCredential = await _auth.signInWithCredential(credential);
+
+      if (userCredential.user != null) {
+        logger.info('OTP verification successful for phone number: $phoneNumber');
+        return true;
+      } else {
+        logger.warning('Failed to verify OTP for phone number: $phoneNumber');
+        return false;
+      }
+    } catch (e) {
+      logger.warning('Failed to verify OTP for phone number: $phoneNumber: ${e.toString()}');
+      return false;
+    }
   }
 }
