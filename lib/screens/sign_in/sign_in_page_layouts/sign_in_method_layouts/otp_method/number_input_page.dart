52a482882962d0a1a4d64311a16f6e6e6632bd49
nailsonseat <aadarshv3@gmail.com>
2023-03-11 20:32:32 +0530

    preserve phone number when going back to edit it

--- lib/screens/sign_in/sign_in_page_layouts/sign_in_method_layouts/otp_method/number_input_page.dart
+++ lib/screens/sign_in/sign_in_page_layouts/sign_in_method_layouts/otp_method/number_input_page.dart
@@ -16,162 +16,209 @@ class NumberInputPage extends StatelessWidget {
     var height = MediaQuery.of(context).size.height;
     var width = MediaQuery.of(context).size.width;
     var textScale = MediaQuery.of(context).textScaleFactor;
-    return Column(
-      children: [
-        Expanded(
-          flex: 1,
-          child: Container(
-            alignment: Alignment.centerLeft,
-            padding: const EdgeInsets.only(left: 25),
-            color: Colors.grey[100],
-            child: FittedBox(
-              fit: BoxFit.scaleDown,
-              child: Padding(
-                padding: EdgeInsets.only(top: height / 95),
-                child: Text('Enter Number',
-                    style: GoogleFonts.quicksand(
-                      fontSize: (height + width) / 50,
-                    )),
-              ),
-            ),
-          ),
-        ),
-        Container(
-          color: Colors.grey[100],
-          child: const Divider(
-            color: Colors.black,
-            thickness: .5,
-          ),
-        ),
-        Expanded(
-            flex: 1,
-            child: Container(
-              alignment: Alignment.topLeft,
-              color: Colors.grey[100],
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.start,
-                children: [
-                  const SizedBox(
-                    width: 10,
-                  ),
-                  TextButton(
-                    onPressed: () {
-                      animatedPageJump("OPTIONS");
-                    },
-                    child: Row(
-                      children: const [
-                        Icon(
-                          Icons.arrow_back_ios,
-                          size: 20,
-                        ),
-                        Text("  Back")
-                      ],
+
+    final PhoneNumberProvider phoneNumberProvider =
+    Provider.of<PhoneNumberProvider>(context, listen: false);
+
+    TextEditingController controller=TextEditingController();
+
+    if (phoneNumberProvider.phoneNumber.length!=3){
+      controller.text=phoneNumberProvider.phoneNumber.substring(3,phoneNumberProvider.phoneNumber.length);
+    }
+
+    return Scaffold(
+      resizeToAvoidBottomInset: true,
+      body: SingleChildScrollView(
+        child: SizedBox(
+          height: height / 2.04,
+          child: Column(
+            children: [
+              Expanded(
+                flex: 1,
+                child: Container(
+                  alignment: Alignment.centerLeft,
+                  padding: const EdgeInsets.only(left: 25),
+                  color: Colors.grey[100],
+                  child: FittedBox(
+                    fit: BoxFit.scaleDown,
+                    child: Padding(
+                      padding: EdgeInsets.only(top: height / 95),
+                      child: Text('Enter Number',
+                          style: GoogleFonts.quicksand(
+                            fontSize: (height + width) / 50,
+                          )),
                     ),
-                  ),
-                ],
-              ),
-            )),
-        Expanded(
-            flex: 1,
-            child: Container(
-              alignment: Alignment.center,
-              color: Colors.grey[100],
-              child: Padding(
-                padding: const EdgeInsets.all(15.0),
-                child: Text(
-                  "This number will be used for OTP verification",
-                  textAlign: TextAlign.center,
-                  style: TextStyle(
-                    fontSize: textScale * 15,
                   ),
                 ),
               ),
-            )),
-        Expanded(
-          flex: 2,
-          child: Container(
-            color: Colors.grey[100],
-            alignment: Alignment.topCenter,
-            child: Padding(
-              padding: const EdgeInsets.all(15.0),
-              child: Row(
-                children: [
-                  Expanded(
-                    flex: 2,
-                    child: Consumer<PhoneNumberProvider>(
-                        builder: (context, countryProvider, child) {
-                      final selectedCountry = countryProvider.selectedCountry ??
-                          countryProvider.countries[0];
-                      return DropdownButton<Country>(
-                        value: selectedCountry,
-                        onChanged: (Country? country) {
-                          if (country != null) {
-                            countryProvider.setSelectedCountry(country);
-                          }
-                        },
-                        items: countryProvider.countries
-                            .map<DropdownMenuItem<Country>>((Country country) {
-                          return DropdownMenuItem<Country>(
-                            value: country,
-                            child: Text('${country.flag} ${country.code}'),
-                          );
-                        }).toList(),
-                      );
-                    }),
-                  ),
-                  /*Expanded(
-                    flex: 5,
-                    child: TextField(
-                      keyboardType: TextInputType.number,
-                      onChanged: (value) {},
-                      inputFormatters: [
-                        FilteringTextInputFormatter.allow(RegExp(r'\d')),
-                      ],
-                      decoration: const InputDecoration(
-                        labelText: "Enter your phone number",
-                        border: OutlineInputBorder(),
+              Container(
+                color: Colors.grey[100],
+                child: const Divider(
+                  color: Colors.black,
+                  thickness: .5,
+                ),
+              ),
+              Container(
+                alignment: Alignment.topLeft,
+                color: Colors.grey[100],
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: [
+                    const SizedBox(
+                      width: 10,
+                    ),
+                    TextButton(
+                      onPressed: () {
+                        animatedPageJump("OPTIONS");
+                      },
+                      child: Row(
+                        children: const [
+                          Icon(
+                            Icons.arrow_back_ios,
+                            size: 20,
+                          ),
+                          Text("  Back")
+                        ],
                       ),
                     ),
-                  ),*/
-                  Consumer<PhoneNumberProvider>(
-                    builder: (context, provider, child) {
-                      return Expanded(
-                        flex: 5,
-                        child: TextField(
-                          keyboardType: TextInputType.number,
-                          onChanged: (value) {
-                            provider.setNumber(value);
-                          },
-                          inputFormatters: [
-                            FilteringTextInputFormatter.allow(RegExp(r'\d')),
-                          ],
-                          decoration: const InputDecoration(
-                            labelText: "Enter your phone number",
-                            border: OutlineInputBorder(),
-                          ),
-                        ),
-                      );
-                    },
-                  )
-                ],
-              ),
-            ),
-          ),
-        ),
-        Row(
-          children: [
-            Container(
-              alignment: Alignment.centerLeft,
-              child: TextButton(
-                onPressed: () {animatedPageJump("OTP2");},
-                child: Row(
-                  children: const [Text("Continue")],
+                  ],
                 ),
               ),
-            ),
-          ],
-        )
-      ],
+              Expanded(
+                  flex: 1,
+                  child: Container(
+                    alignment: Alignment.topCenter,
+                    color: Colors.grey[100],
+                    child: Padding(
+                      padding: const EdgeInsets.all(15.0),
+                      child: Wrap(alignment: WrapAlignment.center, children: [
+                        Text("This number will be used\n for OTP verification",
+                            textAlign: TextAlign.center,
+                            style: GoogleFonts.nunito(
+                              fontSize: textScale * 15,
+                            )),
+                      ]),
+                    ),
+                  )),
+              Expanded(
+                flex: 2,
+                child: Container(
+                  color: Colors.grey[100],
+                  alignment: Alignment.topCenter,
+                  child: Padding(
+                    padding: const EdgeInsets.all(15.0),
+                    child: Row(
+                      children: [
+                        Flexible(
+                          flex: 2,
+                          child: SizedBox(
+                            height: 60,
+                            width: width * 30,
+                            child: Container(
+                              alignment: Alignment.center,
+                              decoration: BoxDecoration(
+                                border: Border.all(
+                                  color: Colors.grey,
+                                  width: 1,
+                                ),
+                                borderRadius: BorderRadius.circular(4),
+                              ),
+                              child: Consumer<PhoneNumberProvider>(
+                                  builder: (context, countryProvider, child) {
+                                final selectedCountry =
+                                    countryProvider.selectedCountry ??
+                                        countryProvider.countries[0];
+                                return Material(
+                                  color: Colors.transparent,
+                                  child: DropdownButton<Country>(
+                                    value: selectedCountry,
+                                    onChanged: (Country? country) {
+                                      if (country != null) {
+                                        countryProvider
+                                            .setSelectedCountry(country);
+                                      }
+                                    },
+                                    underline: Container(),
+                                    items: countryProvider.countries
+                                        .map<DropdownMenuItem<Country>>(
+                                            (Country country) {
+                                      return DropdownMenuItem<Country>(
+                                        value: country,
+                                        child: Row(
+                                          children: [
+                                            const SizedBox(
+                                              width: 6,
+                                            ),
+                                            Text(country.flag),
+                                            const SizedBox(
+                                              width: 5,
+                                            ),
+                                            Text(country.code),
+                                          ],
+                                        ),
+                                      );
+                                    }).toList(),
+                                  ),
+                                );
+                              }),
+                            ),
+                          ),
+                        ),
+                        const SizedBox(
+                          width: 10,
+                        ),
+                        Flexible(
+                          flex: 4,
+                          child: Consumer<PhoneNumberProvider>(
+                            builder: (context, provider, child) {
+                              return TextField(
+                                controller: controller,
+                                keyboardType: TextInputType.number,
+                                onChanged: (value) {
+                                  provider.setNumber(value);
+                                },
+                                inputFormatters: [
+                                  FilteringTextInputFormatter.allow(
+                                      RegExp(r'\d')),
+                                ],
+                                decoration: const InputDecoration(
+                                  labelText: "Enter your phone number",
+                                  border: OutlineInputBorder(),
+                                ),
+                              );
+                            },
+                          ),
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: Container(
+                  color: Colors.grey[100],
+                  padding: const EdgeInsets.fromLTRB(0, 0, 10, 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.end,
+                    children: [
+                      TextButton(
+                        onPressed: () => animatedPageJump("OTP2"),
+                        child: Row(
+                          children: const [
+                            Text("Continue"),
+                            SizedBox(width: 5),
+                            Icon(Icons.arrow_forward_ios, size: 15),
+                          ],
+                        ),
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
